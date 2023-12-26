55cdd4a9c4759246db95dff6d86cbfb323f554c9
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2023-12-26 23:40:51 +0100

    feat: add react-native-safe-area-context

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { Button, Divider } from 'react-native-paper';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export default function App() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View style={styles.container}>
-      <Divider />
-      <Text>Open up App.js to start working on your app!</Text>
-      <Divider />
-      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-        Press me
-      </Button>
-      <Divider />
-      {/* eslint-disable-next-line react/style-prop-object */}
-      <StatusBar style="auto" />
-    </View>
+      <View style={{ flex: 1, paddingTop: insets.top }}>
+        <Divider />
+        <Text>Open up App.js to start working on your app!</Text>
+        <Divider />
+        <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
+        <Divider />
+        {/* eslint-disable-next-line react/style-prop-object */}
+        <StatusBar style="auto" />
+      </View>
   );
 }
